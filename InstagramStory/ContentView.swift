743ae69974243466488672377743f6ba25a37394
erennlk <elik.dark91@icloud.com>
2021-06-11 10:51:56 +0300

    Initial Commit

---
+++ InstagramStory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramStory
+//
+//  Created by Eren  Çelik on 11.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramStory/InstagramStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoryApp.swift
+//  InstagramStory
+//
+//  Created by Eren  Çelik on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
